0b9e0dc311fe10d9bafb84583916b737a69c06ea
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-06-21 15:41:31 -0700

    added ReviewImage migration and model

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -18,6 +18,11 @@ module.exports = (sequelize, DataTypes) => {
       Review.belongsTo(models.User, {
         foreignKey: 'userId'
       })
+
+      Review.hasMany(models.ReviewImage, {
+        foreignKey: 'reviewId',
+        onDelete: 'CASCADE'
+      })
     }
   }
   Review.init({
