5c1dc0919d98a1d51e621612e43d775f3ae058e4
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-06-21 15:14:13 -0700

    added Booking migration and model

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -9,6 +9,11 @@ module.exports = (sequelize, DataTypes) => {
       Spot.belongsTo(models.User, {
         foreignKey: 'ownerId'
       })
+
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE'
+      })
     }
   }
   Spot.init({
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -12,7 +12,13 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, {
-        foreignKey: 'ownerId'
+        foreignKey: 'ownerId',
+        onDelete: 'CASCADE'
+      })
+
+      User.hasMany(models.Booking, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
       })
     }
   }
