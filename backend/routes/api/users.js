aa3f438d34efe88193847774758fb3fdb8e6bb45
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-06-22 11:13:18 -0700

    minor changes for login user and create user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const { Op } = require('sequelize');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models')
@@ -11,14 +12,20 @@ const router = express.Router();
 
 // Validate Signup Middleware
 const validateSignup = [
+    check('firstName')
+        .exists({ checkFalsy: true})
+        .withMessage('First Name is required'),
+    check('lastName')
+        .exists({ checkFalsy: true})
+        .withMessage('Last Name is required'),
     check('email')
         .exists({ checkFalsy: true })
         .isEmail()
-        .withMessage('Please provide a valid email.'),
+        .withMessage('Invalid email'),
     check('username')
         .exists({ checkFalsy: true })
         .isLength({ min: 4 })
-        .withMessage('Please provide a username with at least 4 characters.'),
+        .withMessage('Username is required'),
     check('username')
         .not()
         .isEmail()
@@ -26,13 +33,36 @@ const validateSignup = [
     check('password')
         .exists({ checkFalsy: true })
         .isLength({ min: 6 })
-        .withMessage('Password must be 6 characters or more.'),
+        .withMessage('Password is required'),
     handleValidationErrors
 ];
 
 // Sign up
-router.post('/', validateSignup, async (req, res) => {
+router.post('/', validateSignup, async (req, res, next) => {
     const { email, password, username, firstName, lastName} = req.body;
+
+    // Check if User exist
+    const existingUser = await User.findOne({
+        where: {
+            [Op.or]: {
+                username: username,
+                email: email
+            }
+        }
+    });
+    console.log(existingUser)
+    if (existingUser) {
+        const err = new Error('User already exists');
+        err.title = 'User already exists'
+        err.status = 500;
+        if (existingUser.username === username) {
+            err.errors = { username: 'User with that username already exists'};
+        } else {
+            err.errors = { email: 'User with that email already exists'};
+        }
+        return next(err);
+    }
+
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({ email, firstName, lastName, username, hashedPassword });
 
